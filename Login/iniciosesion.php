5ec7fccde1a39d53c7773a21a50297e86f339336
arnoldo0411 <105952080+arnoldo0411@users.noreply.github.com>
2024-03-07 08:43:40 -0600

    Update iniciosesion

--- Login/iniciosesion.php
+++ Login/iniciosesion.php
@@ -7,14 +7,16 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-
 if (!empty($_POST["login"])) {
     if (!empty($_POST["username"]) && !empty($_POST["password"])) {
         $usuario = $_POST["username"];
         $clave = $_POST["password"];
 
+        // Convertir el nombre de usuario a minúsculas para realizar la comparación exacta
+        $usuario = strtolower($usuario);
+
         // Lógica para verificar las credenciales en la base de datos
-        $stmt = $link->prepare("SELECT * FROM usuarios WHERE NombreUs = ? AND Contraseña = ?");
+        $stmt = $link->prepare("SELECT * FROM usuarios WHERE BINARY NombreUs = ? AND Contraseña = ?");
         $stmt->bind_param("ss", $usuario, $clave);
         $stmt->execute();
         $result = $stmt->get_result();
@@ -28,11 +30,41 @@ if (!empty($_POST["login"])) {
             exit(); // Terminar el script
         } else {
             // Mostrar mensaje de acceso denegado si las credenciales son incorrectas
-            echo '<script>alert("ACCESO DENEGADO");</script>';
+            $message = "¡ACCESO DENEGADO!, INTENTE DE NUEVO.";
+            echo '<script>showMessage("' . $message . '");</script>';
         }
     } else {
         // Mostrar mensaje si el usuario no ingresó un nombre de usuario o contraseña
-        echo '<script>alert("Por favor ingrese usuario y contraseña");</script>';
+        $message = "Por favor ingrese usuario y contraseña";
+        echo '<script>showMessage("' . $message . '");</script>';
     }
 }
 ?>
+
+<!-- Código JavaScript para mostrar la ventana emergente -->
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var message = "<?php echo isset($message) ? $message : '' ?>";
+        showMessage(message);
+    });
+
+    function showMessage(message) {
+        if (message.trim() !== '') {
+            var popup = document.createElement('div');
+            popup.classList.add('popup');
+            popup.innerText = message;
+
+            document.body.appendChild(popup);
+
+            // Centrar el popup
+            popup.style.top = '50%';
+            popup.style.left = '50%';
+            popup.style.transform = 'translate(-50%, -50%)';
+
+            // Cerrar el popup después de 3 segundos
+            setTimeout(function() {
+                popup.remove();
+            }, 3000);
+        }
+    }
+</script>
