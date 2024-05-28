16596406154731d449e9b8bd2201005ad5d351c8
arnoldo0411 <105952080+arnoldo0411@users.noreply.github.com>
2024-05-28 09:43:48 -0600

    Create indexGlobalCuenta.php

---
+++ css/indexGlobalCuenta.php
@@ -0,0 +1,140 @@
+body {
+    margin: 0;
+    line-height: normal;
+  }
+  
+  :root {
+    /* fonts */
+    --font-poppins: Poppins;
+    --font-agency-fb: "Agency FB";
+    --font-heebo: Heebo;
+    --typography-body-1: Roboto;
+    --button-l: Inter;
+  
+    /* font sizes */
+    --button-l-size: 18px;
+    --body-medium-400-size: 14px;
+    --font-size-9xl: 28px;
+    --font-size-2xs: 11px;
+    --typography-body-1-size: 16px;
+    --font-size-xl: 20px;
+    --heading-04-size: 24px;
+    --font-size-mini: 15px;
+    --body-small-500-size: 12px;
+    --font-size-lgi: 19px;
+    --font-size-3xl: 22px;
+    --font-size-4xl: 23px;
+    --font-size-14xl: 33px;
+    --font-size-7xl: 26px;
+  
+    /* Colors */
+    --color-darkslateblue: #503e9d;
+    --gray-white: #fff;
+    --color-coral: #fb6d3a;
+    --gray-50: #f5f7fa;
+    --color-whitesmoke: #f6f6f5;
+    --color-whitesmoke-100: #f1f1ef;
+    --gray-100: #e9eaf0;
+    --gray-900: #1d2026;
+    --color-gray: #181b1a;
+    --color-gray-300: rgba(255, 255, 255, 0.12);
+    --color-gray-400: rgba(255, 255, 255, 0.15);
+    --light-action-hover-4p: rgba(0, 0, 0, 0.04);
+    --color-gray-100: rgba(0, 0, 0, 0.87);
+    --color-gray-200: rgba(0, 0, 0, 0.6);
+    --color-slateblue: #6553b3;
+    --color-silver: #b3b6b6;
+    --color-black: #000;
+    --color-lightgreen: #64cf6f;
+    --color-dodgerblue: #1976d2;
+    --color-mediumslateblue: #7b61ff;
+    --primary-500: #ff6636;
+    --gray-600: #6e7485;
+    --color-dimgray-100: #6a6f79;
+    --gray-700: #4e5566;
+    --gray-500: #8c94a3;
+    --color-lightslategray-100: #8692a6;
+    --primary-100: #ffeee8;
+    --success-500: #23bd33;
+    --color-mistyrose: #e6d4cf;
+    --color-seagreen: #348f6c;
+  
+    /* Gaps */
+    --gap-21xl: 40px;
+    --gap-41xl: 60px;
+    --gap-9xs: 4px;
+    --gap-61xl: 80px;
+    --gap-10xs: 3px;
+    --gap-3xs: 10px;
+    --gap-smi: 13px;
+    --gap-xl: 20px;
+    --gap-xs: 12px;
+    --gap-13xl: 32px;
+    --gap-base: 16px;
+    --gap-5xl: 24px;
+    --gap-7xs: 6px;
+    --gap-5xs: 8px;
+    --gap-4xl: 23px;
+    --gap-6xs: 7px;
+    --gap-43xl: 62px;
+    --gap-12xl: 31px;
+    --gap-mini: 15px;
+    --gap-9xl: 28px;
+    --gap-11xs: 2px;
+    --gap-7xs-5: 5.5px;
+    --gap-8xs: 5px;
+    --gap-7xl: 26px;
+    --gap-131xl: 150px;
+  
+    /* Paddings */
+    --padding-21xl: 40px;
+    --padding-4xs: 9px;
+    --padding-6xs: 7px;
+    --padding-5xs: 8px;
+    --padding-3xl: 22px;
+    --padding-13xl: 32px;
+    --padding-xl: 20px;
+    --padding-8xs: 5px;
+    --padding-12xs: 1px;
+    --padding-12xl: 31px;
+    --padding-smi: 13px;
+    --padding-lgi: 19px;
+    --padding-2xs: 11px;
+    --padding-mid: 17px;
+    --padding-10xs: 3px;
+    --padding-xs: 12px;
+    --padding-9xs: 4px;
+    --padding-5xl: 24px;
+    --padding-11xs: 2px;
+    --padding-7xs: 6px;
+    --padding-lg: 18px;
+    --padding-33xl: 52px;
+    --padding-20xl: 39px;
+    --padding-15xl: 34px;
+    --padding-6xl: 25px;
+    --padding-4xl: 23px;
+    --padding-5xs-5: 7.5px;
+    --padding-base: 16px;
+    --padding-43xl: 62px;
+    --padding-41xl: 60px;
+    --padding-9xl: 28px;
+    --padding-2xl: 21px;
+  
+    /* Border radiuses */
+    --br-5xl: 24px;
+    --br-3xs: 10px;
+    --br-29xl: 48px;
+    --br-base: 16px;
+    --br-xl: 20px;
+    --br-11xs: 2px;
+    --br-21xl: 40px;
+    --br-9xs: 4px;
+    --br-45xl: 64px;
+    --br-8xs: 5px;
+    --br-11xl: 30px;
+  
+    /* Effects */
+    --line-bottom: 0px -1px 0px #e9eaf0 inset;
+    --dropdown: 0px 6px 16px rgba(0, 0, 0, 0.06);
+  }
+  
